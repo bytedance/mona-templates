3f1cfe943d7ab26068d0cf077344731828f4d8ff
xwchris <zhangxiaowei.chris@bytedance.com>
2021-11-12 14:13:15 +0800

    chore: update tpl

--- plugin/src/pages/Home/index.tsx
+++ plugin/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from '@bytedance/mona-runtime'
-import logo from '@/common/imgs/logo.svg';
+import Logo from '@/common/imgs/logo.svg';
 import Wrapper from '@/components/Wrapper';
 import styles from './index.module.<%= data.cssExt %>';
 
@@ -8,13 +8,13 @@ const Home = () => {
   return (
     <Wrapper>
       <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
+        <Logo className={styles.logo} />
         <p>Edit <code>src/containers/Home/index.tsx</code> and save to reload</p>
         <ul className={styles.list}>
           <li className={styles.link}><Link to="/pages/Info/index">Click me to navigate <code>/pages/Info/index</code></Link></li>
           <li className={styles.link}><Link to="/pages/List/index">Click me to navigate <code>/pages/List/index</code></Link></li>
           <li className={styles.link}>
-            <Link to='/pages/SdkExample/index'>示例程序</Link>
+            <Link to='/pages/SdkExample/index'>API调用示例程序</Link>
           </li>
         </ul>
       </header>
