9d090215f655a77e627862c970ffd4101f8f7a81
fengbo <fengbo.lucky@bytedance.com>
2021-11-25 14:45:34 +0800

    chore: plugin test

--- __test__/plugin.test.ts
+++ __test__/plugin.test.ts
@@ -1,27 +1,33 @@
 import path from 'path';
 import { execSync } from 'child_process';
 import { processTemplates } from './template';
+import fse from 'fs-extra';
+const tmpDir = path.join(__dirname, `../${new Date().valueOf()}`);
 test('process Js Templates', async () => {
+  await fse.copy(path.join(__dirname, '../plugin'), tmpDir);
   expect(
     await catchError(() =>
-      processTemplates(path.join(__dirname, '../plugin'), {
+      processTemplates(tmpDir, {
         projectName: 'monaTest',
         cssExt: 'less',
-        typescript: false
+        typescript: true
       })
     )
   ).toBe(false);
 }, 100000);
 
 test('template build', async () => {
-  expect(
-    await catchError(() =>
-      execSync(
-        `cd ${path.join(__dirname, '../plugin')} && npm install --registry=https://registry.npmjs.org && npm run build`,
-        { encoding: 'utf8' }
+  try {
+    expect(
+      await catchError(() =>
+        execSync(`cd ${tmpDir} && npm install --registry=https://registry.npmjs.org && npm run build`, {
+          encoding: 'utf8'
+        })
       )
-    )
-  ).toBe(false);
+    ).toBe(false);
+  } finally {
+    await fse.remove(tmpDir);
+  }
 }, 100000);
 
 const catchError = async (fn: any) => {
