b077a523e00eb2e296fa9fd8c87c8bb346af3308
fengbo <fengbo.lucky@bytedance.com>
2022-02-22 17:45:35 +0800

    chore(app): app.tsx

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import '@/common/styles/base.<%= data.cssExt %>';
+import React, { useReducer } from 'react';
 
-const App<%- data.typescript ? ': React.FC' : '' %> = ({ children }) => {
-  return (
-    <div>{children}</div>
-  )
-}
+const initData = ['mona'];
+
+<% if(data.typescript) { %>
+export const TodoContext = React.createContext<{ state: string[]; dispatch: any }>({ state: [], dispatch: () => {} });
+
+const reducer = (state: string[], action: any): string[] => {
+<%} else {%>
+export const TodoContext = React.createContext({ state: [], dispatch: () => {} });
+
+const reducer = (state, action) => {
+<%} %>
+  switch (action.type) {
+    case 'add':
+      const { data } = action.payload;
+      return [...state, data];
+    case 'delete':
+      const { index } = action.payload;
+      state.splice(index, 1);
+      return [...state];
+    default:
+      return state;
+  }
+};
+const App: React.FC = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initData);
+  return <TodoContext.Provider value={{ state, dispatch }}>{children}</TodoContext.Provider>;
+};
 
 export default App;
