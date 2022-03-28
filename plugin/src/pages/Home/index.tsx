8b5618cb5b34050a73df6c167bba7e8d47db925c
xwchris <zhangxiaowei.chris@bytedance.com>
2022-03-28 15:14:20 +0800

    chore: update plugin logo

--- plugin/src/pages/Home/index.tsx
+++ plugin/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from '@bytedance/mona-runtime'
-import Logo from '@/common/imgs/logo.svg';
+import logo from '@/common/imgs/logo.svg';
 import Wrapper from '@/components/Wrapper';
 import styles from './index.module.<%= data.cssExt %>';
 
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <Wrapper>
       <header className={styles.header}>
-        <Logo className={styles.logo} />
+        <img className={styles.logo} src={logo} />
         <p>Edit <code>src/pages/Home/index<%- data.typescript ? '.tsx' : '.jsx' %></code> and save to reload</p>
         <ul className={styles.list}>
           <li className={styles.link}><Link to="/pages/Info/index">Click me to navigate <code>/pages/Info/index</code></Link></li>
