5bd0f1d2fc6620248f9a6e9655f61793649997ee
zhaochenting <zhaochenting@bytedance.com>
2022-03-28 11:22:09 +0800

    feat: max template change

--- max/src/components/Hello.tsx
+++ max/src/components/Hello.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export default () => {
-    return (
-        <div>
-            <div>Hello Max Isv Component!!!</div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>Hello Max Isv Component!!!</div>
+    </div>
+  );
+};
