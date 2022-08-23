d77f93dc5d5bf57a77dd8a3e3bfa291b8b12a948
xwchris <zhangxiaowei.chris@bytedance.com>
2022-08-23 15:50:28 +0800

    chore: add overflow auto

--- light/src/app.tsx
+++ light/src/app.tsx
@@ -3,7 +3,7 @@ import '@/common/styles/base.<%= data.cssExt %>';
 
 const App<%- data.typescript ? ': React.FC' : '' %> = ({ children }) => {
   return (
-    <div style={{ height: '100%' }}>{children}</div>
+    <div style={{ height: '100%', overflow: 'auto' }}>{children}</div>
   )
 }
 
